9f3bc64f88493ab2e54fa9c37174c4a3cb5bfc75
Gih2139Sanches <giovanaclemente2139@gmail.com>
2025-04-06 14:55:07 -0300

    :sparkles: feat: adicionando taskcontroller na camada de controller

--- src/main/java/com/nexum/kanban/controller/TaskController.java
+++ src/main/java/com/nexum/kanban/controller/TaskController.java
@@ -1,2 +1,37 @@
-package com.nexum.kanban.controller;public class TaskController {
+package com.nexum.kanban.controller;
+
+import com.nexum.kanban.model.Task;
+import com.nexum.kanban.service.TaskService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
+public class TaskController {
+
+    private final TaskService taskService;
+
+    @GetMapping
+    public List<Task> getAll() {
+        return taskService.findAll();
+    }
+
+    @PostMapping
+    public Task create(@RequestBody Task task) {
+        return taskService.save(task);
+    }
+
+    @PutMapping("/{id}")
+    public Task update(@PathVariable Long id, @RequestBody Task task) {
+        return taskService.update(id, task);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        taskService.delete(id);
+    }
 }
