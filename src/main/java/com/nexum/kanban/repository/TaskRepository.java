3d5b4485855df139b75823ddf2b4aeb9c155e584
Gih2139Sanches <giovanaclemente2139@gmail.com>
2025-04-06 14:45:59 -0300

    :sparkles: feat: adicionando repositorio da tarefa

--- src/main/java/com/nexum/kanban/repository/TaskRepository.java
+++ src/main/java/com/nexum/kanban/repository/TaskRepository.java
@@ -1,2 +1,13 @@
-package com.nexum.kanban.repository;public interface TaskRepository {
+package com.nexum.kanban.repository;
+
+import com.nexum.kanban.model.Status;
+import com.nexum.kanban.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByStatus(Status status);
 }
