3076e90da78a6378578085f1c0abba6c8f127cc4
Gih2139Sanches <giovanaclemente2139@gmail.com>
2025-04-06 14:53:15 -0300

    :sparkles: feat: adicionando camada de service

--- src/main/java/com/nexum/kanban/service/TaskService.java
+++ src/main/java/com/nexum/kanban/service/TaskService.java
@@ -1,2 +1,36 @@
-package com.nexum.kanban.service;public class TaskService {
+package com.nexum.kanban.service;
+
+import com.nexum.kanban.model.Task;
+import com.nexum.kanban.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class TaskService {
+
+    private final TaskRepository repository;
+
+    public List<Task> findAll() {
+        return repository.findAll();
+    }
+
+    public Task save(Task task) {
+        return repository.save(task);
+    }
+
+    public Task update(Long id, Task updatedTask) {
+        Task task = repository.findById(id).orElseThrow();
+        task.setTitle(updatedTask.getTitle());
+        task.setDescription(updatedTask.getDescription());
+        task.setDueDate(updatedTask.getDueDate());
+        task.setStatus(updatedTask.getStatus());
+        return repository.save(task);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
 }
