9bba199b5fbac85088c16208f84309ef0c4db6cd
Gih2139Sanches <giovanaclemente2139@gmail.com>
2025-04-06 21:44:56 -0300

    :recycle: refactor: adaptar TaskController para uso de DTOs

--- src/main/java/com/nexum/kanban/controller/TaskController.java
+++ src/main/java/com/nexum/kanban/controller/TaskController.java
@@ -1,37 +1,49 @@
 package com.nexum.kanban.controller;
 
-import com.nexum.kanban.model.Task;
+import com.nexum.kanban.dto.TaskDTO;
 import com.nexum.kanban.service.TaskService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/tasks")
-@RequiredArgsConstructor
-@CrossOrigin(origins = "*")
+
+@CrossOrigin(origins = "*") // Libera para frontend
 public class TaskController {
 
     private final TaskService taskService;
+    @Autowired
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
 
     @GetMapping
-    public List<Task> getAll() {
-        return taskService.findAll();
+    public ResponseEntity<List<TaskDTO>> getAllTasks() {
+        return ResponseEntity.ok(taskService.getAllTasks());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TaskDTO> getTaskById(@PathVariable Long id) {
+        return ResponseEntity.ok(taskService.getTaskById(id));
     }
 
     @PostMapping
-    public Task create(@RequestBody Task task) {
-        return taskService.save(task);
+    public ResponseEntity<TaskDTO> createTask(@RequestBody TaskDTO dto) {
+        return ResponseEntity.ok(taskService.createTask(dto));
     }
 
     @PutMapping("/{id}")
-    public Task update(@PathVariable Long id, @RequestBody Task task) {
-        return taskService.update(id, task);
+    public ResponseEntity<TaskDTO> updateTask(@PathVariable Long id, @RequestBody @Validated TaskDTO dto) {
+        return ResponseEntity.ok(taskService.updateTask(id, dto));
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) {
-        taskService.delete(id);
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+        return ResponseEntity.noContent().build();
     }
 }
